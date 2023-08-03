c4c0af96b68f25ecd02569e3652c5081a8652040
Ghisea Stefan <ghiseastefan@gmail.com>
2023-08-03 03:33:56 +0300

    lazy loading

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,22 @@
-import Home from '../Home/Home';
-import Project from '../Project/Project';
-import AppLayout from './AppLayout';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+import AppLayout from './AppLayout';
 import projects from '../Project/projects.json';
+
+const Project = React.lazy(() => import('../Project/Project'))
 
 const App: React.FC = () => {
   return (
     <AppLayout>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        {projects.map((project) => (
-          <Route path={project.path} element={<Project header={project.header} desc={project.desc} gitPath={project.gitPath} items={project.items} />} />
-        ))}
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          {projects.map((project) => (
+            <Route path={project.path} element={<Project header={project.header} desc={project.desc} gitPath={project.gitPath} items={project.items} />} />
+          ))}
+        </Routes>
+      </Suspense>
     </AppLayout>
   )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Intro from '../Home/Intro/Intro'
 import ScrollingInfo from '../Home/ScrollingInfo/ScrollingInfo';
-import About from '../Home/About/About';
-import ProjectsPreview from '../Home/ProjectsPreview/ProjectsPreview';
-import Skills from '../Home/Skills/Skills';
+const About = React.lazy(() => import('./About/About'))
+const ProjectsPreview = React.lazy(() => import('./ProjectsPreview/ProjectsPreview'));
+const Skills = React.lazy(() => import('./Skills/Skills'));
 
 const Home: React.FC = () => {
     return (
         <>
             <Intro />
             <ScrollingInfo />
-            <Skills />
-            <About />
-            <ProjectsPreview />
+            <Suspense fallback={<></>}>
+                <Skills />
+            </Suspense>
+            <Suspense fallback={<></>}>
+                <About />
+            </Suspense>
+            <Suspense fallback={<></>}>
+                <ProjectsPreview />
+            </Suspense>
         </>
     )
 }
